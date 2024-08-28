fb2d9d044c94e53aa658eb22f836fc0248621f41
aditya <adityapandeyadp@gmail.com>
2024-08-29 04:16:16 +0530

    sdnfbonfdajl

--- router/Note.js
+++ router/Note.js
@@ -6,13 +6,13 @@ const add = require("../controllers/note/new");
 const add2 = require("../controllers/note/newcs");
 const get2 = require("../controllers/note/all2");
 
-router.get("/fetchallpynote",get );
-router.get("/fetchallnotes2",get2 );
+router.get("/fetchallpy",get );
+router.get("/fetchallcss",get2 );
 
 
 
 router.post("/addpy", add);
-router.post("/addpy", add2);
+router.post("/addcs", add2);
 
 
 module.exports = router;
