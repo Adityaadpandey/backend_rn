466caf336f1d20bd06eebcbd04873b99839d81b3
aditya <adityapandeyadp@gmail.com>
2024-08-31 15:47:54 +0530

    signin avatar

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -1,44 +1,57 @@
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const JWT_SECRECT = "Aditya";
+const JWT_SECRET = "Aditya";
 
 const loged = async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
-  
+
   if (user) {
     return res
       .status(400)
-      .json({ error: "Sorry a user with this email already exists." });
+      .json({ error: "Sorry, a user with this email already exists." });
   }
+
   let name = await User.findOne({ name: req.body.name });
   if (name) {
     return res
       .status(400)
-      .json({ error: "Sorry a user with this user-name already exists." });
+      .json({ error: "Sorry, a user with this username already exists." });
   }
+
   try {
-    //  slating
-    password = req.body.password;
+    // Hashing the password
+    const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    nPass = await bcrypt.hash(password, salt);
-    //  adding to the database
-    var seed = Math.floor(Math.random() * 5000)
-    var img = `https://avatars.dicebear.com/api/human/${seed}.svg`;
-    user = await User.create({ name: req.body.name, email: req.body.email, password: nPass, img: img,  });
-    // to generate token
+    const nPass = await bcrypt.hash(password, salt);
+
+    // Generating a unique avatar URL using Dicebear
+    const seed = Math.floor(Math.random() * 5000);
+    const img = `https://avatars.dicebear.com/api/adventurer-neutral/${seed}.svg`;
+
+    // Creating the user and storing it in the database
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: nPass,
+      img: img,
+    });
+
+    // Generating a JWT token
     const data = {
       user: {
         id: user.id,
-      }
-    }
-    const token = jwt.sign(data, JWT_SECRECT);
-    let success = true
-    // sending the token
-    res.json({ success, token, img, name: req.body.name}).status(200);
+      },
+    };
+    const token = jwt.sign(data, JWT_SECRET);
+    const success = true;
+
+    // Sending the response with the token and user information
+    res.status(200).json({ success, token, img, name: req.body.name });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
 
 module.exports = loged;
